3c6c6ba5457eb4129c8b379dc894e8877c9a7e4f
Дмитрий Похода <dmitrijpohoda2940@gmail.com>
2025-06-13 13:24:49 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,13 @@
+function sayHello() {
+  const output = document.getElementById("output");
+  output.textContent = "Привет! Ты нажал кнопку!";
+}
+
+function openSite() {
+  window.location.href = "https://yandex.ru/images/";
+}
+
+function button_contacts() {
+  window.location.href = "contacts.html";
+}
+
